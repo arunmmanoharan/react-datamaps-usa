a0fa6e0bda2af76229719b53d7426132207ed633
Arun Manohar <arunmanohar.manoharan@cms.hhs.gov>
2019-09-21 18:56:50 -0400

    Add code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import StateLabels from './StateLabels';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+	const clickedState = (e) => {
+		console.log(e)
+	}
+
+	return (
+		<StateLabels clickedState={clickedState}/>
+	);
+};
 
 export default App;
